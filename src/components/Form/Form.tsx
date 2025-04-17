0547303824318be4982ac4d8e416d471d0e2f541
Dasha <darina1970@mail.ru>
2025-04-18 00:55:26 +0700

    add props and hooks

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,11 +1,25 @@
+import { useState } from "react";
 import "./Form.scss";
 
-export const Form = () => {
+export const Form = (props: { createNewToDo: Function }) => {
+  const [text, setText] = useState<string>("");
+
+  const formSubmit = () => {
+    if (text) {
+      props.createNewToDo(text);
+      setText("");
+    }
+  };
+
   return (
     <div className="form-wrapper">
-      <form action="#">
+      <form action="#" onSubmit={formSubmit}>
         <label>
-          <input type="text" />
+          <input
+            type="text"
+            value={text}
+            onChange={(event) => setText(event.target.value)}
+          />
           <button></button>
         </label>
       </form>
--- src/pages/toDoListPage.tsx
+++ src/pages/toDoListPage.tsx
@@ -1,13 +1,51 @@
 import { Header } from "../components/Header/Header";
 import { Form } from "../components/Form/Form";
 import { ToDoList } from "../components/ToDoList/ToDoList";
+import { toDo } from "../models/todo-item";
+import { useState } from "react";
 
 export const ToDoListPage = () => {
+  const [todos, setTodos] = useState<toDo[]>([
+    {
+      id: 0,
+      text: "Первая задача",
+      isDone: false,
+    },
+    {
+      id: 1,
+      text: "Вторая задача",
+      isDone: true,
+    },
+    {
+      id: 2,
+      text: "Третья задача",
+      isDone: true,
+    },
+  ]);
+
+  const createNewToDo = (text: string) => {
+    const newToDo: toDo = {
+      id: todos.length,
+      text: text,
+      isDone: false,
+    };
+    setTodos([...todos, newToDo]);
+  };
+
+  const updateToDo = (toDoItem: toDo) => {
+    console.log("update");
+    console.log(toDoItem);
+  };
+
+  const deleteToDo = (toDoItem: toDo) => {
+    console.log("delete");
+    console.log(toDoItem);
+  };
   return (
     <>
       <Header />
-      <Form />
-      <ToDoList />
+      <Form createNewToDo={createNewToDo} />
+      <ToDoList todos={todos} updateToDo={updateToDo} deleteToDo={deleteToDo} />
     </>
   );
 };
